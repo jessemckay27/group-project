82a2083b0e39043b97c2d3009cfb4bed61ac7784
Jacob <jacobhixon47@gmail.com>
2016-11-08 11:29:22 -0800

    Add name input for orders, add confirmation page.

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
   $("form#menu-form").submit(function(event) {
     event.preventDefault();
 
-    $("#orders-list").append("<div class='order'>" + "<i class='glyphicon glyphicon-remove-sign'></i>" + "<li>" + "<ul>" + "</ul>" + "</li>" + "</div>")
+    $("#orders-list").append("<div class='order'>" + "<i class='glyphicon glyphicon-remove-sign'></i>" + "<li>" + $("input#order-name-input").val().toUpperCase() + "<ul>" + "</ul>" + "</li>" + "</div>")
 
     $("input:checked").each(function() {
       $("#orders-list ul").last().append("<li>" + $(this).parent().text() + "</li>")
@@ -18,6 +18,17 @@ $(document).ready(function() {
     });
 
     $("input:checkbox").prop("checked", false);
+    $("input#order-name-input").val("")
+    $("#submit-orders-button").show();
+  });
+
+  $("#submit-orders-button").click(function(event) {
+    event.preventDefault();
+
+    $("#order-confirmation-column").append($("#orders-list"));
+    $("i").remove();
+    $("#menu-page-row").slideUp();
+    $("#order-confirmation-row").slideDown();
   });
 
 
