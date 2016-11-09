63bfe78bf0f9e76159f0d15c9734ccecfa39ed0e
Dave Denlow and Jesse McKay <jesse@>
2016-11-08 16:40:33 -0800

    add login functionality

--- js/scripts.js
+++ js/scripts.js
@@ -5,8 +5,10 @@ function Customer(userName, guests, date) {
   this.pin = number(0,9) + number(0,9) + number(0,9) + number(0,9);
 }
 
-Customer.prototype.checkValue = function() {
-
+Customer.prototype.pinCheck = function() {
+  if(this.pin === myPin) {
+    alert("this works!");
+  }
 }
 
 var number = function(min, max) {
@@ -14,23 +16,59 @@ var number = function(min, max) {
 }
 
 $(document).ready(function() {
-  $("form").submit(function(event) {
+  $("form#one").submit(function(event) {
     event.preventDefault();
 
     var inputUserName = $("#name input").val();
     var inputGuests = $("#guests select").val();
     var inputDate = $("#date input").val();
     var myTable = new Customer(inputUserName, inputGuests, inputDate);
+    console.log(myTable);
+    $("form#two").submit(function(event) {
+      event.preventDefault();
 
+      var myPin = $("#pin input").val();
+      $("#submit-fix-pin").click(function(){
+        myTable.pinCheck();
+      });
+    });
   });
 
-  $("#name div").click(function() {
+
+
+
+
+
+  $("#register").click(function() {
+    $("#landing").hide();
+    $("#name").fadeIn(500);
+  });
+
+  $("#name .submit").click(function() {
       if($("#name input").val().length <= 2) {
-        alert("Please enter a name")
+        $(this).addClass("error-warning");
       } else {
         $("#name").hide();
-        $("#guests").show();
+        $("#guests").fadeIn(300);
       }
+    });
 
+  $("#guests .submit").click(function() {
+    $("#guests").hide();
+    $("#date").fadeIn(300);
   });
+
+  $("#date button").click(function() {
+    $("#date").hide();
+    $("#landing").fadeIn(300);
+  });
+
+  $("#sign-in").click(function(){
+    $("#landing").hide();
+    $("#pin").show();
+  });
+
+
+
+
 });
