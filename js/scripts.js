1ed1069d6bbc718b310425883ec5238e6433c75d
Jacob Hixon and Justin Xu <justin@>
2016-11-08 16:39:35 -0800

    Add table total to confirmation page.

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,15 @@
 
 // User Int Logic //
 $(document).ready(function() {
+  var tableTotal = 0
   $("form#menu-form").submit(function(event) {
     event.preventDefault();
 
-    $("#orders-list").append("<div class='order'>" + "<i class='glyphicon glyphicon-remove-sign'></i>" + "<li>" + $("input#order-name-input").val().toUpperCase() + "<ul>" + "</ul>" + "</li>" + "</div>")
+    $("#orders-list").append("<div class='order'>" + "<i class='glyphicon glyphicon-remove-sign'></i>" + "<li>" + $("input#order-name-input").val().toUpperCase() + "<ul>" + "</ul>" + "</li>" + "</div>");
 
     $("input:checked").each(function() {
-      $("#orders-list ul").last().append("<li>" + $(this).parent().text() + "</li>")
+      $("#orders-list ul").last().append("<li>" + $(this).parent().text() + "</li>");
+      tableTotal += parseInt($(this).val());
     });
 
     $("i").click(function() {
@@ -24,13 +26,13 @@ $(document).ready(function() {
 
   $("#submit-orders-button").click(function() {
     $("#order-confirmation-list").append($("#orders-list").children());
+    $("span#table-total").empty();
+    $("span#table-total").append(tableTotal);
     $("i").remove();
     $("#menu-page-row").slideUp(function() {
       $("#submit-orders-button").hide();
     });
     $("#order-confirmation-row").slideDown();
-
-
   });
 
   $("#order-more-button").click(function() {
@@ -39,6 +41,5 @@ $(document).ready(function() {
     });
 
   });
-
 
 });
